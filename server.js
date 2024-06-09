3b302c7e2168fc281d4682344bc6a5d8b5a60bc9
J3anlUcas <160613430+J3anlUcas@users.noreply.github.com>
2024-06-09 17:36:00 -0300

    organizando os diretorios

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+//const path = require('path')
+const app = express()
+const routes = require('./src/routes/homeRoutes.js')
+app.use(express.json())
+
+const { SERVER_PORT } = process.env;
+
+// app.set('views', path.resolve(__dirname, './src/views'))    
+// app.set('view engine', 'ejs')
+
+app.use(routes)
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Servidor ON!🚀`)
+})  
