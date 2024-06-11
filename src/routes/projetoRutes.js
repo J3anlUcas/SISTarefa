9c9e14dae4846f0ee87021fdd1cef3164c4b0154
Jean Lucas <jeanlucas2005@hotmail.com>
2024-06-11 15:05:20 -0300

    .

--- src/routes/projetoRutes.js
+++ src/routes/projetoRutes.js
@@ -4,10 +4,10 @@ const route = express.Router()
 const crudprojeto = require('../modules/Projeto/crudProjeto')
 const autenticaçãoToken = require('../middlewares/validarToken')
 
-route.post("/project",autenticaçãoToken.validarToken, crudprojeto.create)
-route.delete("/project",autenticaçãoToken.validarToken, crudprojeto.delete)
-route.get("/projects", autenticaçãoToken.validarToken, crudprojeto.read)
-route.put("/project", autenticaçãoToken.validarToken, crudprojeto.update)
+route.post("/project",/*autenticaçãoToken.validarToken,*/ crudprojeto.create)
+route.delete("/project",/*autenticaçãoToken.validarToken,*/ crudprojeto.delete)
+route.get("/projects", /*autenticaçãoToken.validarToken,*/ crudprojeto.read)
+route.put("/project", /*autenticaçãoToken.validarToken,*/ crudprojeto.update)
 
 
 module.exports = route
